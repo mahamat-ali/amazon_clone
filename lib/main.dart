85117982eb2efbd636a14bb1f6883ca7126ccfcc
Mahamat Ali <terapfils30@gmail.com>
2022-06-19 17:03:12 +0100

    added auth files and folders

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:amazon_clone/constants/global_variables.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -11,14 +12,31 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Amazon Clone',
-      theme: ThemeData(),
+      theme: ThemeData(
+        scaffoldBackgroundColor: GlobalVariables.backgroundColor,
+        colorScheme: const ColorScheme.light(
+          primary: GlobalVariables.secondaryColor,
+        ),
+        appBarTheme: const AppBarTheme(
+          elevation: 0,
+          iconTheme: IconThemeData(color: Colors.black),
+        ),
+      ),
       home: Scaffold(
         appBar: AppBar(
           title: const Text('Amazon Clone'),
         ),
-        body: const SafeArea(
+        body: SafeArea(
           child: Center(
-            child: Text('Amazon clone'),
+            child: Column(
+              children: [
+                ElevatedButton(
+                  onPressed: () {},
+                  child: const Text('Click'),
+                ),
+                const Text('Amazon clone'),
+              ],
+            ),
           ),
         ),
       ),
