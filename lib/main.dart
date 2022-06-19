642c42dc44395f17d6d99bc8f62354eda7a80db9
Mahamat Ali <terapfils30@gmail.com>
2022-06-19 16:44:44 +0100

    First commit on test_one

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-import 'package:amazon_clone/constants/global_variables.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -12,31 +11,14 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Amazon Clone',
-      theme: ThemeData(
-        scaffoldBackgroundColor: GlobalVariables.backgroundColor,
-        colorScheme: const ColorScheme.light(
-          primary: GlobalVariables.secondaryColor,
-        ),
-        appBarTheme: const AppBarTheme(
-          elevation: 0,
-          iconTheme: IconThemeData(color: Colors.black),
-        ),
-      ),
+      theme: ThemeData(),
       home: Scaffold(
         appBar: AppBar(
           title: const Text('Amazon Clone'),
         ),
-        body: SafeArea(
+        body: const SafeArea(
           child: Center(
-            child: Column(
-              children: [
-                ElevatedButton(
-                  onPressed: () {},
-                  child: const Text('Click'),
-                ),
-                const Text('Amazon clone'),
-              ],
-            ),
+            child: Text('Amazon clone'),
           ),
         ),
       ),
